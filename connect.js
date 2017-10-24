409451a5a8f321352982f1601752ca0bec2c7738
meg768 <meg768@users.noreply.github.com>
2017-10-24 15:53:42 +0200

    -

--- connect.js
+++ connect.js
@@ -1,3 +1,21 @@
+var piWifi = require('pi-wifi');
+
+var networkDetails = {
+  ssid: 'Julia',
+  password: 'potatismos'
+};
+
+//A simple connection
+piWifi.connectTo(networkDetails, function(err) {
+  if(!err) {
+    console.log('Network created successfully!');
+  } else {
+    console.log(err.message); //Failed to connect
+  }
+});
+
+/*
+
 var wpa_supplicant = require('wireless-tools/wpa_supplicant');
 
 var options = {
@@ -12,3 +30,4 @@ wpa_supplicant.enable(options, function(err) {
     console.log(err);
   // connected to the wireless network
 });
+*/
