35c1f45d2ad20146a17fd1136a7f8206a6c95bf3
meg768 <meg768@users.noreply.github.com>
2017-10-25 12:03:45 +0200

    -

--- wifi.js
+++ wifi.js
@@ -202,6 +202,8 @@ class WiFi {
                 var match;
                 var status = {};
 
+                console.log(output);
+
                 if ((match = output.match(/[^b]ssid=([^\n]+)/))) {
                     status.ssid = match[1];
                 }
@@ -209,6 +211,7 @@ class WiFi {
                 if ((match = output.match(/ip_address=([^\n]+)/))) {
                     status.ip_address = match[1];
                 }
+                console.log(status);
 
                 resolve(status);
             })
