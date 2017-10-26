0550097964861372814ceccaf9453d44e0006055
meg768 <meg768@users.noreply.github.com>
2017-10-26 14:35:38 +0200

    -

--- install.js
+++ install.js
@@ -1,66 +1,105 @@
 
-var WiFi = require('./wifi.js');
+var WiFi = require('./wifi-connection.js');
 var isString = require('yow/is').isString;
+var Events  = require('events');
 
+class WifiSetup extends Events {
 
-function install(fileName) {
-    var fs = require('fs');
-
-    function loadFile() {
-        try {
-            return JSON.parse(fs.readFileSync(fileName));
-        }
-        catch(error) {
-            console.log(error);
-        }
-    }
-
-    function deleteFile() {
-        try {
-            //fs.unlinkSync(fileName);
-        }
-        catch(error) {
-            console.log(error);
-        }
+    constructor() {
+        super();
 
     }
 
-    return new Promise((resolve, reject) => {
-        var wifi = new WiFi();
+    setup(fileName) {
+        var fs = require('fs');
 
-        wifi.getConnectionState().then((connected) => {
-            if (!connected) {
-                console.log('Not connected, trying to connect');
+        function loadFile() {
+            try {
+                return JSON.parse(fs.readFileSync(fileName));
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
 
-                var config = loadFile();
+        function deleteFile() {
+            try {
+                //fs.unlinkSync(fileName);
+            }
+            catch(error) {
+                console.log(error);
+            }
 
-                if (config && isString(config.ssid)) {
-                    console.log('Trying to connect to ', config.ssid);
-                    return wifi.connectToNetwork(config.ssid, config.password);
+        }
+
+        return new Promise((resolve, reject) => {
+            var wifi = new WiFi();
+
+            wifi.getConnectionState().then((connected) => {
+                if (!connected) {
+                    console.log('Not connected, trying to connect');
+
+                    var config = loadFile();
+
+                    if (config && isString(config.ssid)) {
+                        this.emit('connecting');
+
+                        console.log('Trying to connect to ', config.ssid);
+
+                        return wifi.connectToNetwork(config.ssid, config.password).then(() => {
+                            this.emit('connected');
+
+                        });
+                    }
+                    else {
+                        console.log('No network credentials in config file.');
+                        return Promise.resolve();
+                    }
                 }
                 else {
-                    console.log('No network credentials in config file.');
+                    console.log('Already connected!');
                     return Promise.resolve();
                 }
-            }
-            else {
-                console.log('Already connected!');
-                return Promise.resolve();
-            }
-        })
-        .then(() => {
-            deleteFile();
-            resolve();
-        })
-        .catch((error) => {
-            reject(error);
-        });
+            })
+            .then(() => {
+                deleteFile();
+                this.emit('done', error);
+                resolve();
+            })
+            .catch((error) => {
+                this.emit('error', error);
+                reject(error);
+            });
 
-    });
+        });
+    }
+
+    setup() {
+
+    }
 }
 
+var setup = new WifiSetup();
 
-install('/boot/bluetooth/config.json').then(() => {
+setup.on('connecting', () => {
+    console.log('connecting');
+});
+
+setup.on('done', () => {
+    console.log('done!');
+});
+
+setup.on('connected', () => {
+    console.log('done!');
+});
+
+setup.on('error', (error) => {
+    console.log('ERROR!!');
+    console.log(error);
+});
+
+
+setup.setup('/boot/bluetooth/config.json').then(() => {
     console.log('Done!');
 
 })
